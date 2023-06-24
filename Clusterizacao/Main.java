aca38cda66db2e2c5bcc47a53190dc12c63cb46d
Evellyn Freitas <80793635+evellynfreitas@users.noreply.github.com>
2023-06-24 00:45:07 -0300

    Método Main

---
+++ Clusterizacao/Main.java
@@ -0,0 +1,35 @@
+package Clusterizacao;
+
+public class Main {
+	
+	public static void main(String args[]) {
+		
+		int n = 10;
+		Ponto[] pontos = new Ponto[n];
+		for(int i=0; i<n; i++) pontos[i] = new Ponto(n);
+		
+		int i=0;
+		for(int f=1; f<n; f++)
+			i+=f;
+		
+		
+		Diferenca[] diferencas = new Diferenca[i];
+		int index = -1;
+		
+		for(int a=0; a<n-1; a++)
+		{
+			for(int b=a+1; b<n; b++)
+			{
+				index++;
+				diferencas[index] = new Diferenca(pontos[a], pontos[b]);
+				System.out.println("A: " + diferencas[index].getA() + " B: " + diferencas[index].getB() + " Dif: " + diferencas[index].getDiferenca());
+			}
+		}
+		/*
+		System.out.println(index);
+		System.out.println(i);
+		for(int f=0; f<index; f++) System.out.println("A: " + diferencas[f].getA() + " B: " + diferencas[f].getB() + " Dif: " + diferencas[f].getDiferenca());
+		*/
+	}
+
+}
